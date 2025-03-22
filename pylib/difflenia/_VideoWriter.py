08a91272330b0c00056d7c773740effac126b976
mmore500 <mmore500.login@gmail.com>
2025-03-22 20:36:50 +0000

    Deploying to binder-dev-pathsplot.ipynb from @ mmore500/hstrat-agentless-concept@acaeb503803f3c0944d89602a14532042b7df405 🚀

--- pylib/difflenia/_VideoWriter.py
+++ pylib/difflenia/_VideoWriter.py
@@ -87,7 +87,7 @@ class VideoWriter:
     def __enter__(self):
         return self
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, traceback):
         self.close()
 
     def show(self):
