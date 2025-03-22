8f11fe8e60050865de925a50126799ea305cdfc2
mmore500 <mmore500.login@gmail.com>
2025-03-22 22:08:47 +0000

    Deploying to binder-dev-difflenia-AttributeBoxMax.ipynb from @ mmore500/hstrat-agentless-concept@f569efc9bd302113bce36d2ffbb9f76e348df98a 🚀

---
+++ pylib/_pathsplot.py
@@ -0,0 +1,58 @@
+import typing
+
+import pandas as pd
+import seaborn as sns
+
+from ._dewrap_path import dewrap_path
+
+
+def pathsplot(
+    paths: typing.List[typing.List[typing.Tuple[int, int]]],
+    xmax: typing.Optional[int] = None,
+    ymax: typing.Optional[int] = None,
+    **kwargs: dict,
+) -> sns.FacetGrid:
+    """Plot multiple paths.
+
+    Parameters
+    ----------
+    paths : List[List[Tuple[int, int]]]
+        List of paths to plot.
+
+    kwargs
+        Additional keyword arguments to pass to seaborn.relplot.
+
+    Returns
+    -------
+    sns.FacetGrid
+        Seaborn FaceGrid object.
+    """
+    path_dfs = []
+    for p, path in enumerate(paths):
+        for path in dewrap_path(path, xmax, ymax):
+            t, x, y = zip(*path)
+            t, x, y = [*t], [*x], [*y]
+            df = pd.concat(
+                [
+                    pd.DataFrame({"x": x[:-1], "y": y[:-1], "t": t[:-1]}),
+                    pd.DataFrame({"x": x[1:], "y": y[1:], "t": t[:-1]}),
+                ],
+            )
+            df["path"] = p
+            path_dfs.append(df)
+
+    df = pd.concat(path_dfs)
+
+    g = sns.relplot(
+        data=df,
+        x="x",
+        y="y",
+        hue="t",
+        col="path",
+        estimator=None,
+        kind="line",
+        sort=False,
+        **kwargs,
+    )
+    g.set(xlim=(0, xmax), ylim=(0, ymax))
+    return g
