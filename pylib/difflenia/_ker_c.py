5b7fc189ae52518c349f1837c639fa8982a93f12
mmore500 <mmore500.login@gmail.com>
2025-03-23 14:43:34 +0000

    Deploying to binder-dev-synthgeom.ipynb from @ mmore500/hstrat-agentless-concept@c1d01cbad7c6b03f638246b587e96db339b1f55b 🚀

---
+++ pylib/difflenia/_ker_c.py
@@ -0,0 +1,7 @@
+# adapted from
+# https://developmentalsystems.org/sensorimotor-lenia
+import torch
+
+
+def ker_c(x, r, w, b):
+    return (b * torch.exp(-(((x.unsqueeze(-1) - r) / w) ** 2) / 2)).sum(-1)
