b43a941dad7cf96b170b2d9cd5c6cd2a863f6f6a
mmore500 <mmore500.login@gmail.com>
2025-03-24 18:09:57 +0000

    Deploying to binder-dev-synthgeom-AttributeBoxMax.ipynb from @ mmore500/hstrat-agentless-concept@8bedebafa01fbb54a0b966cc9da08cb35a9e0224 🚀

--- tests/test_auxlib/test_make_dstream_validation_dataframe.py
+++ tests/test_auxlib/test_make_dstream_validation_dataframe.py
@@ -7,61 +7,68 @@ from pylib.auxlib._make_dstream_validation_dataframe import (
     make_dstream_validation_dataframe,
 )
 
-# def test_make_dstream_validation_dataframe():
-#     S = 64
 
-#     # block validation setup
-#     block_validator = (
-#         f"pl.col('dstream_value') == pl.col('dstream_Tbar') // {S // 2 + 1}"
-#     )
+def test_make_dstream_validation_dataframe():
+    S = 64
+    dstream_algo = "dstream.tilted_algo"
 
-#     def block_differentia_override(T):
-#         return T // (S // 2 + 1)
+    # block validation setup
+    block_validator = (
+        f"pl.col('dstream_value') == pl.col('dstream_Tbar') // {S // 2 + 1}"
+    )
 
-#     block_df1 = make_dstream_validation_dataframe(
-#         differentia_override=block_differentia_override,
-#         validator=block_validator,
-#         S=S,
-#         T=64,
-#     )
-#     block_df2 = make_dstream_validation_dataframe(
-#         differentia_override=block_differentia_override,
-#         validator=block_validator,
-#         S=S,
-#         T=65,
-#     )
+    def block_differentia_override(T):
+        return T // (S // 2 + 1)
 
-#     # checkerboard validation setup
-#     checkerboard_validator = (
-#         "pl.col('dstream_value') == pl.col('dstream_Tbar') % 2"
-#     )
+    block_df1 = make_dstream_validation_dataframe(
+        differentia_override=block_differentia_override,
+        validator=block_validator,
+        S=S,
+        T=64,
+        dstream_algo=dstream_algo,
+    )
+    block_df2 = make_dstream_validation_dataframe(
+        differentia_override=block_differentia_override,
+        validator=block_validator,
+        S=S,
+        T=65,
+        dstream_algo=dstream_algo,
+    )
 
-#     def checkerboard_differentia_override(T):
-#         return T % 2
+    # checkerboard validation setup
+    checkerboard_validator = (
+        "pl.col('dstream_value') == pl.col('dstream_Tbar') % 2"
+    )
 
-#     checkerboard_df1 = make_dstream_validation_dataframe(
-#         differentia_override=checkerboard_differentia_override,
-#         validator=checkerboard_validator,
-#         S=S,
-#         T=64,
-#     )
-#     checkerboard_df2 = make_dstream_validation_dataframe(
-#         differentia_override=checkerboard_differentia_override,
-#         validator=checkerboard_validator,
-#         S=S,
-#         T=65,
-#     )
+    def checkerboard_differentia_override(T):
+        return T % 2
 
-#     df = pd.concat([block_df1, block_df2, checkerboard_df1, checkerboard_df2])
+    checkerboard_df1 = make_dstream_validation_dataframe(
+        differentia_override=checkerboard_differentia_override,
+        validator=checkerboard_validator,
+        S=S,
+        T=64,
+        dstream_algo=dstream_algo,
+    )
+    checkerboard_df2 = make_dstream_validation_dataframe(
+        differentia_override=checkerboard_differentia_override,
+        validator=checkerboard_validator,
+        S=S,
+        T=65,
+        dstream_algo=dstream_algo,
+    )
 
-#     res = hstrat_reconstruct_dataframe(df)
-#     assert len(res) >= len(df)
-#     assert "ancestor_id" in res.columns
-#     assert "id" in res.columns
+    df = pd.concat([block_df1, block_df2, checkerboard_df1, checkerboard_df2])
+
+    res = hstrat_reconstruct_dataframe(df)
+    assert len(res) >= len(df)
+    assert "ancestor_id" in res.columns
+    assert "id" in res.columns
 
 
 def test_make_dstream_validation_dataframe_trivial():
     S = 64
+    dstream_algo = "dstream.tilted_algo"
 
     # trivial validator 0 setup
     trivial0_validator = "pl.col('dstream_value') == 0"
@@ -74,12 +81,14 @@ def test_make_dstream_validation_dataframe_trivial():
         validator=trivial0_validator,
         S=S,
         T=64,
+        dstream_algo=dstream_algo,
     )
     trivial0_df2 = make_dstream_validation_dataframe(
         differentia_override=trivial_differentia_override0,
         validator=trivial0_validator,
         S=S,
         T=65,
+        dstream_algo=dstream_algo,
     )
 
     # trivial validator 1 setup
@@ -93,12 +102,14 @@ def test_make_dstream_validation_dataframe_trivial():
         validator=trivial1_validator,
         S=S,
         T=64,
+        dstream_algo=dstream_algo,
     )
     trivial1_df2 = make_dstream_validation_dataframe(
         differentia_override=trivial_differentia_override1,
         validator=trivial1_validator,
         S=S,
         T=65,
+        dstream_algo=dstream_algo,
     )
 
     df = pd.concat([trivial0_df1, trivial0_df2, trivial1_df1, trivial1_df2])
