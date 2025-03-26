7139c59797f0634081d6f1178962fef97ba07d76
mmore500 <mmore500.login@gmail.com>
2025-03-26 16:02:28 +0000

    Deploying to binder-dev-difflenia-AttributeBoxMax.ipynb from @ mmore500/hstrat-agentless-concept@995b85b550fc115787e8195fa68be4f3e847243c 🚀

--- replit.nix
+++ replit.nix
@@ -1,15 +1,7 @@
 { pkgs }: {
     deps = [
       pkgs.htop
-      pkgs.python311Packages.pytest
-      pkgs.python310Packages.pytest
       pkgs.vim
-      pkgs.pandoc
-      pkgs.texlive.combined.scheme-full
-      pkgs.python39Packages.pip
-      pkgs.editorconfig-checker
-      pkgs.python39Packages.editorconfig
-      pkgs.cowsay
       pkgs.bibtex-tidy
     ];
 }
