8058c82e8eb26461d8e8893acae4686aeda327a4
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-10-23 07:33:26 -0400

    Draft gosper wse visualization nb

--- requirements.sh
+++ requirements.sh
@@ -4,3 +4,4 @@ set -e
 
 cd "$(dirname "$0")"
 python3 -m uv pip compile requirements.in > requirements.txt
+python3 -m uv pip compile --python-version "3.11" requirements_py311.in > requirements_py311.txt
